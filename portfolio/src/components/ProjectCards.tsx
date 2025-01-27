1089cf69d0b0bbeb2d7741e4d1549d128470edd0
Edwin-Najera <leonajera0930@gmail.com>
2025-01-27 13:35:06 -0600

    Wrong ccode input into StartPage and ProjectCards

--- portfolio/src/components/ProjectCards.tsx
+++ portfolio/src/components/ProjectCards.tsx
@@ -1,36 +1,54 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
-import AboutMe from "./components/AboutMe";
-import luffy from "./assets/Photos/Subject.png";
-import StartPage from "./components/StartPage";
-import ProjectCards from "./components/ProjectCards";
-import image1 from "./assets/Photos/Hasbulla.jpeg";
-import image2 from "./assets/Photos/Subject.png";
-import image3 from "./assets/Photos/luffy.jpg";
+import "./ProjectCards.css";
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-
-      <section>
-        <StartPage image={luffy} />
-      </section>
-
-      <section id="AboutMe">
-        <AboutMe imageSrc={luffy} />
-      </section>
-
-      <section id="Projects" className="in-body header">
-        <h2>Projects</h2>
-        <ProjectCards image1={image1} image2={image2} image3={image3} />
-      </section>
-
-      <section id="Contact" className="in-body header">
-        <h2>Contact</h2>
-      </section>
-    </div>
-  );
+interface Props {
+  image1: string;
+  image2: string;
+  image3: string;
 }
 
-export default App;
+const ProjectCards = ({ image1, image2, image3 }: Props) => {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <div className="container-line">
+      <div className="card btn" onClick={handleClick}>
+        <img className="card-img" src={image1} />
+        <div className="card-body">
+          <h4 className="card-title">Title</h4>
+          <p className="card-description">Description</p>
+        </div>
+      </div>
+      <div className="card btn" onClick={handleClick}>
+        <img className="card-img" src={image2} />
+        <a className="card-body">
+          <h4 className="card-title">Title</h4>
+          <p className="card-description">Description</p>
+        </a>
+      </div>
+      <div className="card btn" onClick={handleClick}>
+        <img className="card-img" src={image3} />
+        <div className="card-body">
+          <h4 className="card-title">Title</h4>
+          <p className="card-description">Description</p>
+        </div>
+      </div>
+      <div className="container-stack">
+        <div className="card-small btn" onClick={handleClick}>
+          <div className="card-body-small">
+            <h4 className="card-title-small">C++ Mini Projects</h4>
+            <p className="card-description-small">Description</p>
+          </div>
+        </div>
+        <div className="card-small btn" onClick={handleClick}>
+          <div className="card-body-small">
+            <h4 className="card-title-small">Java Mini Projects</h4>
+            <p className="card-description-small">Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCards;
--- portfolio/src/components/StartPage.tsx
+++ portfolio/src/components/StartPage.tsx
@@ -1,36 +1,32 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
-import AboutMe from "./components/AboutMe";
-import luffy from "./assets/Photos/Subject.png";
-import StartPage from "./components/StartPage";
-import ProjectCards from "./components/ProjectCards";
-import image1 from "./assets/Photos/Hasbulla.jpeg";
-import image2 from "./assets/Photos/Subject.png";
-import image3 from "./assets/Photos/luffy.jpg";
+import { Fragment } from "react";
+import "./StartPage.css";
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-
-      <section>
-        <StartPage image={luffy} />
-      </section>
-
-      <section id="AboutMe">
-        <AboutMe imageSrc={luffy} />
-      </section>
-
-      <section id="Projects" className="in-body header">
-        <h2>Projects</h2>
-        <ProjectCards image1={image1} image2={image2} image3={image3} />
-      </section>
-
-      <section id="Contact" className="in-body header">
-        <h2>Contact</h2>
-      </section>
-    </div>
-  );
+interface Props {
+  image: string;
 }
+const StartPage = ({ image }: Props) => {
+  return (
+    <Fragment>
+      <div className="container">
+        <div>
+          <h1 className="text-left">Edwin </h1>
+          <div className="text-left-bottom">
+            Leonardo
+            <br />
+            Coder/Designer
+          </div>
+        </div>
+        <img src={image} className="image" alt="Profile-Image" />
+        <div>
+          <h1 className="text-right"> Najera</h1>
+          <div className="text-right-bottom">
+            Gonzalez <br />
+            CS Undergraduate
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-export default App;
+export default StartPage;
