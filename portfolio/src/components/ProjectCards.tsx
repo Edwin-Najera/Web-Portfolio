0139249a0659874bdd9911c612ffd7eba52ff5f7
Edwin-Najera <leonajera0930@gmail.com>
2025-01-29 11:01:31 -0600

    Removed "import React from {react}"

--- portfolio/src/components/ProjectCards.tsx
+++ portfolio/src/components/ProjectCards.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ProjectCards = () => {
   return <div>ProjectCards</div>;
 };
--- portfolio/src/components/StartPage.tsx
+++ portfolio/src/components/StartPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const StartPage = () => {
   return <div>StartPage</div>;
 };
