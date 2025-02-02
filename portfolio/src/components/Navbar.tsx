52cb71fd2740cfcc7dbb592aec74af1ab1986586
Edwin-Najera <leonajera0930@gmail.com>
2025-02-01 21:08:01 -0600

    Added a navbar, imported bootstrap, changes to css

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -1,7 +1,13 @@
-import "/global.css";
+import "./global.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="sticky-top">
+      <nav className="navbar">
+        <h4 className="btn">Edwin Najera</h4>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
