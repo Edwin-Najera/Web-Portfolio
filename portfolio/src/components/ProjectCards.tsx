ff8a46e2d3d86827a867caeb72d1b87f04711cca
Edwin-Najera <leonajera0930@gmail.com>
2025-02-03 01:48:05 -0600

    Photos added and ProjectCards modified

--- portfolio/src/components/ProjectCards.tsx
+++ portfolio/src/components/ProjectCards.tsx
@@ -3,7 +3,7 @@ import "./global.css";
 const ProjectCards = () => {
   return (
     <div className="row">
-      <div className="col-sm-6 mb-3 mb-sm-0">
+      <div className="col-sm-3">
         <div className="card">
           <img className="card-img-top" src="src" alt="alt" />
           <div className="card-body">
@@ -12,11 +12,25 @@ const ProjectCards = () => {
           </div>
         </div>
       </div>
-      <div className="col-sm-6">
+      <div className="col-sm-3">
         <div className="card">
           <img className="card-img-top" src="src" alt="alt" />
           <div className="card-body">
             <h5 className="card-title">Project 2</h5>
+            <p className="card-text">This is some text for the card for now</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-3">
+        <div className="card mb-3">
+          <div className="card-body">
+            <h5 className="card-title">Java Projects</h5>
+            <p className="card-text">This is some text for the card for now</p>
+          </div>
+        </div>
+        <div className="card ">
+          <div className="card-body">
+            <h5 className="card-title">C++ Projects</h5>
             <p className="card-text">This is some text for the card for now</p>
           </div>
         </div>
