52e46962edf6bb3ced13ec80a9b873ee33cf09c8
Edwin-Najera <leonajera0930@gmail.com>
2025-02-10 17:08:22 -0600

    Maintenace for Maintenance Branch

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,26 +1,12 @@
 import { Fragment } from "react/jsx-runtime";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import ProjectCards from "./components/ProjectCards";
-import AboutMe from "./components/AboutMe";
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
-      <div id="About Me">
-        <h2 className="title-text">About Me</h2>
-        <AboutMe />
-      </div>
-
-      <div id="projects">
-        <h2 className="title-text">Projects</h2>
-        <ProjectCards />
-      </div>
-
-      <div id="contact">
-        <h2 className="title-text">Contact Me</h2>
-      </div>
+      <h1 id="Maintenance">This Site is Under Maintenance</h1>
     </Fragment>
   );
 };
