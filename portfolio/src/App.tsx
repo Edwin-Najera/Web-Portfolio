6cb2f136842e9aa4a1d27ed7cccac617323d8739
Edwin-Najera <leonajera0930@gmail.com>
2025-03-28 14:35:37 -0500

    Now footer is properly pushed into repo

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,11 @@
 import { Fragment } from "react/jsx-runtime";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ProjectCards from "./components/ProjectCards";
 import AboutMe from "./components/AboutMe";
 import StartPage from "./components/StartPage";
 import Contact from "./components/Contact";
-import Footer from "./components/footer";
 
 const App = () => {
   return (
@@ -33,7 +33,31 @@ const App = () => {
           <Contact />
         </div>
         <div id="Footer">
-          <Footer />
+          <footer>
+            <div className="d-flex justify-content-between bg-dark" id="footer">
+              <div className="">Developed by Edwin Najera</div>
+              <div className="" style={{ justifySelf: "right" }}>
+                <a
+                  className="nav-link d-inline"
+                  aria-current="page"
+                  href="https://github.com/Edwin-Najera"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub size={30} />
+                </a>
+                <a
+                  className="nav-link d-inline"
+                  aria-current="page"
+                  href="https://www.linkedin.com/in/edwin-najera-49767a30b"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin size={30} />
+                </a>
+              </div>
+            </div>
+          </footer>
         </div>
       </div>
     </Fragment>
