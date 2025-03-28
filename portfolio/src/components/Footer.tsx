6529ac7adad00a1930291e4ba6c1eda39d69f02c
Edwin-Najera <leonajera0930@gmail.com>
2025-03-28 14:32:23 -0500

    Footer properly pushed

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -1,31 +1,33 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
-const Footer = () => {
+const footer = () => {
   return (
-    <div className="d-flex justify-content-between bg-dark" id="footer">
-      <div className="">Developed by Edwin Najera</div>
-      <div className="" style={{ justifySelf: "right" }}>
-        <a
-          className="nav-link d-inline"
-          aria-current="page"
-          href="https://github.com/Edwin-Najera"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub size={30} />
-        </a>
-        <a
-          className="nav-link d-inline"
-          aria-current="page"
-          href="https://www.linkedin.com/in/edwin-najera-49767a30b"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin size={30} />
-        </a>
+    <footer>
+      <div className="d-flex justify-content-between bg-dark" id="footer">
+        <div className="">Developed by Edwin Najera</div>
+        <div className="" style={{ justifySelf: "right" }}>
+          <a
+            className="nav-link d-inline"
+            aria-current="page"
+            href="https://github.com/Edwin-Najera"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={30} />
+          </a>
+          <a
+            className="nav-link d-inline"
+            aria-current="page"
+            href="https://www.linkedin.com/in/edwin-najera-49767a30b"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={30} />
+          </a>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
-export default Footer;
+export default footer;
