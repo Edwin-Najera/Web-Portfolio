fd394525f08dfbbd2f89a0a7bb63c91b3a6d5417
Edwin-Najera <leonajera0930@gmail.com>
2025-04-02 12:48:21 -0500

    New background colors

--- portfolio/src/components/StartPage.tsx
+++ portfolio/src/components/StartPage.tsx
@@ -1,13 +1,17 @@
+import { Fragment } from "react/jsx-runtime";
 import "./global.css";
 
 const StartPage = () => {
   return (
-    <div className="container">
-      <div className="side-img">
-        Hi my name is
-        <h1 className="img-text">Edwin Najera</h1>
+    <Fragment>
+      <div className="d-block-flex Intro">
+        <div className="side-img">
+          Hi my name is
+          <h1 className="img-text">Edwin Najera</h1>
+        </div>
       </div>
-    </div>
+      <div className="bottom" />
+    </Fragment>
   );
 };
 
